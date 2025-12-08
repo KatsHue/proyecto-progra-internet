c520d770bb84d82d077999ce28ede1bf74b530fb
KatsHue <katia.shuerta@gmail.com>
2025-12-08 16:26:02 -0600

    botón de inicio agregado

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
   return (
-    <div className="flex justify-center">
+    <Link to="/" className="flex justify-center">
       <img
         src="/assets/logo2-1.png"
         alt="LogoTipo"
-        className="w-20 h-auto mx-auto"
+        className="w-20 h-auto mx-auto cursor-pointer hover:opacity-80 transition"
       />
-    </div>
+    </Link>
   );
 }
